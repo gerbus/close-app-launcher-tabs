b28c7c7dfa1177a8ad64ea608e12fa2b36024352
gerbus <chris.gerber@jane.app>
2022-03-29 21:30:34 -0700

    Check for existing values onInstalled (fires when Chrome updates) to preserve user config

--- popup.js
+++ popup.js
@@ -2,17 +2,17 @@ const delayElement = document.getElementById("delay")
 const urlsElement = document.getElementById("urls")
 const historyElement = document.getElementById("history")
 
-chrome.storage.sync.get("autoCloseTabDelayInSeconds", data => delayElement.value = data.autoCloseTabDelayInSeconds)
-chrome.storage.sync.get("autoCloseTabURLs", data => urlsElement.value = data.autoCloseTabURLs)
+chrome.storage.sync.get("closeDelayInSeconds", data => delayElement.value = data.closeDelayInSeconds)
+chrome.storage.sync.get("closeURLs", data => urlsElement.value = data.closeURLs)
 chrome.storage.sync.get("history", data => historyElement.value = data.history)
 
 delayElement.addEventListener("input", e => {
     console.log(e.target.value)
-    const autoCloseTabDelayInSeconds = parseInt(e.target.value)
-    chrome.storage.sync.set({ autoCloseTabDelayInSeconds })
+    const closeDelayInSeconds = parseInt(e.target.value)
+    chrome.storage.sync.set({ closeDelayInSeconds })
 })
 urlsElement.addEventListener("input", e => {
     console.log(e.target.value)
-    const autoCloseTabURLs = e.target.value
-    chrome.storage.sync.set({ autoCloseTabURLs })
+    const closeURLs = e.target.value
+    chrome.storage.sync.set({ closeURLs })
 })
